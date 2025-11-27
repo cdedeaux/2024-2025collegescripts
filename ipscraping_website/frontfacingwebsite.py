04067a9a4b32683c86b98ec1282b6686a59b5614
Cameron <camerondedeaux@gmail.com>
2025-11-27 00:17:39 -0600

    Initial startup for IP scraper.

---
+++ ipscraping_website/frontfacingwebsite.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+import sys
+class Database:
+    def __init__(self, db_name = 'ips.db'):
+        self.db_name = db_name
+        try:
+            connection = sqlite3.connect(self.db_name)
+
+        except:
+            print("Failed database connection.")
+
+            
+            sys.exit()
+        cur = connection.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS ip(ip_address TEXT UNIQUE)")
+        connection.commit()
+        connection.close()
+
+app = Flask(__name__)
+@app.route('/')
+def home():
+    user_ip = request.remote_addr
+    db = Database()
+    connection = sqlite3.connect(db.db_name)
+    cur = connection.cursor()
+    try:
+        query = "INSERT INTO ip (ip_address) VALUES (?)"
+        cur.execute(query, (user_ip,))
+        connection.commit()
+        connection.close()
+    except sqlite3.IntegrityError:
+        pass
+    
+    return render_template('frontend.html', ip_address=user_ip)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
